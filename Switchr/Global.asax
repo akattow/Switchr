d51c3b309b499bdcafdbb91a326909b20979fbd7
Kat Tow <kattow88@gmail.com>
2019-01-02 11:59:24 -0600

    Initial commit

---
+++ Switchr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Switchr.MvcApplication" Language="C#" %>
---
+++ Switchr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Switchr.Startup))]
+namespace Switchr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
